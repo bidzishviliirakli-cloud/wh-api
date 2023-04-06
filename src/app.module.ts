aab405b8fc802c562843cf00660006a810871004
Ikamean <ika.bidzishvili@gmail.com>
2023-04-06 13:10:31 +0400

    added client folder to server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from "@nestjs/common";
 import { ContentModule } from "@content";
 import { StrapiModule } from "@strapi";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
-	imports: [ContentModule, StrapiModule]
+	imports: [ServeStaticModule.forRoot({
+		rootPath: join(__dirname, '..', 'client'),
+		//FIXME fix path for server and Client serving
+	}),
+		ContentModule, StrapiModule]
 })
-export class AppModule {}
+export class AppModule { }
