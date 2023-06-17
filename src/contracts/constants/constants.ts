4c5a50dc7e0acf520e77e47f4846e09bb74673fd
ikamean <ika.bidizshvili@gmail.com>
2023-06-17 11:32:59 +0400

    strapi url changed

--- src/contracts/constants/constants.ts
+++ src/contracts/constants/constants.ts
@@ -1,4 +1,4 @@
-export const STRAPI_URL = "http://localhost:1337/api";
+export const STRAPI_URL = "https://warm-house-strapi.herokuapp.com/api";
 
 export const SWAGGER_PROPERTIES = {
 	path: "api",
